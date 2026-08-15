fa73dd8f0a046c4fa7b3fdeb05cfcf152a0ff5e2
PiyushHadape <piyushhadape9@gmail.com>
2026-08-15 22:54:22 +0530

    added

--- script.js
+++ script.js
@@ -5,3 +5,5 @@ prompt("Hello, World!");
 console.log("this is a test");
 console.log("this is a test msg");
 console.log("this is another console msg");
+let message = "Hello, World!";
+console.log(message);
